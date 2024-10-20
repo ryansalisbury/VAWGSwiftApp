6da3df6e3bffc262ff4100a7d9d097e820b0685a
Ryan Salisbury <rxs1035@student.bham.ac.uk>
2024-10-20 14:05:30 +0100

    initial swift boilerplate app commit

---
+++ VAWGApplication/VAWGApplication/VAWGApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  VAWGApplicationApp.swift
+//  VAWGApplication
+//
+//  Created by Ryan Salisbury on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct VAWGApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
