5e4207ed808d0e26a18ff2a4f94d8d183c503a37
Ryan Salisbury <rxs1035@student.bham.ac.uk>
2024-12-13 16:50:48 +0000

    WIP commit

--- VAWGApplication/VAWGApplication/Views/FabIcon/FabIcon.swift
+++ VAWGApplication/VAWGApplication/Views/FabIcon/FabIcon.swift
@@ -7,21 +7,47 @@
 import SwiftUI
 
 struct FabIcon: View {
+    @State private var isFabOpen = true
+
+    func toggleFab() {
+        isFabOpen.toggle()
+    }
+
     var body: some View {
-        Button {
-            // action
-        } label: {
-            Image(systemName: "apple.image.playground")
-                .font(.title.weight(.semibold))
+        VStack {
+            if isFabOpen {
+                Button {
+                    // action
+                } label: {
+                    Image(systemName: "numbers.rectangle")
+                        .font(.title.weight(.semibold))
+                        .padding().background(Color.yellow)
+                        .foregroundColor(.white).clipShape(.circle).shadow(radius: 4, x: 0, y: 4)
+                }
+                Button {
+                    // action
+                } label: {
+                    Image(systemName: "cloud.rainbow.crop.fill")
+                        .font(.title.weight(.semibold))
+                        .padding().background(Color.yellow)
+                        .foregroundColor(.white).clipShape(.circle).shadow(radius: 4, x: 0, y: 4)
+                }
+            }
+            Button {
+                toggleFab()
+            } label: {
+                Image(systemName: "apple.image.playground")
+                    .font(.title.weight(.semibold))
                     .padding().background(Color.orange)
                     .foregroundColor(.white).clipShape(.circle).shadow(radius: 4, x: 0, y: 4)
+            }
         }
     }
-}
 
-// this is purely for viewing purposes
-struct FabIcon_Previews: PreviewProvider {
-    static var previews: some View {
-        FabIcon()
+    // this is purely for viewing purposes
+    struct FabIcon_Previews: PreviewProvider {
+        static var previews: some View {
+            FabIcon()
+        }
     }
 }
